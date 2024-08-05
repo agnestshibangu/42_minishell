0d2f8790e5fdc009b3a3a204789b0fa54126ad20
Thomas Sion <thsion@c1r2p10.42perpignan.fr>
2024-08-05 15:20:50 +0200

    au propre

---
+++ src/prompt.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prompt.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thsion <thsion@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/05 15:18:14 by thsion            #+#    #+#             */
+/*   Updated: 2024/08/05 15:18:17 by thsion           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+int	display_prompt(void)
+{
+	// char	*ret;
+	// int		p_fd[2];
+
+	// if (pipe(p_fd) == -1)
+	// 	return (1);
+	while (1)
+	{
+		ft_putstr_fd(">", 0);
+		// ret = get_next_line(0);
+		// if (ft_strncmp(ret, av[2], ft_strlen(av[2])) == 0
+		// 	&& ret[ft_strlen(av[2])] == '\n')
+		// {
+		// 	free(ret);
+		// 	break ;
+		// }
+		// write(p_fd[1], ret, ft_strlen(ret));
+		// free(ret);
+	}
+	// close(p_fd[1]);
+	// dup2(p_fd[0], 0);
+	// close(p_fd[0]);
+	return (0);
+}
