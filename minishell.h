8abae058af6a8499fee581a9ee6b7f8297ef43a2
Agnes Tshibangu <agnes.tshibangugrillon@gmail.com>
2024-08-17 17:43:31 +0200

    make changes

--- minishell.h
+++ minishell.h
@@ -6,14 +6,12 @@
 # include <readline/history.h>
 # include <unistd.h>
 # include <stdlib.h>
-# include <string.h>
 # include <errno.h>
 # include <sys/types.h>
 # include <sys/wait.h>
 # include <signal.h>
 # include <fcntl.h>
 # include <stdbool.h>
-# include "libft/libft.h"
 
 
 # ifndef BUFFER_SIZE
@@ -97,6 +95,43 @@ void signal_handler(void);
 void new_routine(int signal);
 void	heredoc_signal(void);
 void	heredoc_signal_handler(int signal);
+
+
+int		ft_atoi(char *str);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
+void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+char	*ft_itoa(int n);
+char	**ft_split(char const *s, char c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlen(const char *s);
+
 
 
 #endif 
